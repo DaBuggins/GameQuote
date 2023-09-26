2528939aab1e93c60619bf703c056d1bbe0e8fe9
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2023-09-26 12:00:07 +0100

    Update GameQuote.jsx
    added title, character and year of release

--- GameQuote.jsx
+++ GameQuote.jsx
@@ -21,9 +21,12 @@ export default function GameQuote() {
       <p className="Loader" style={{ opacity: isLoading ? 1 : 0 }}>
         Loading...
       </p>
-      <h1>"{quote.quote}"</h1>
+      <h1>{quote.title} ({quote.release})</h1>
+
+
+      <h2>"{quote.quote}"</h2>      
      
-      <h2>- {quote.character}, {quote.title}</h2>
+      <i><h3>-  {quote.character} </h3></i>
     </div>
   );
 }

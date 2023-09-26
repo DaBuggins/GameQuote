7ba0522660d531b3ea276747724371bd8ee08257
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2023-09-26 09:35:59 +0100

    Game quote fetcher to test API calls

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import QuoteFetcherLoader from "./QuoteFetcherLoader";
+import GameQuote from "./GameQuote";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <QuoteFetcherLoader /> */}
+      <GameQuote />
+    </div>
+  );
+}
---
+++ GameQuote.jsx
@@ -0,0 +1,29 @@
+const { useState, useEffect } = require("react");
+const RANDOM_QUOTE_URL = "https://www.ultima.rest/api/random";
+
+export default function GameQuote() {
+  const [quote, setQuote] = useState({ quote: "", title: "" });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchQuote() {
+      const response = await fetch(RANDOM_QUOTE_URL);
+      const jsonResponse = await response.json();
+      const randomQuote = jsonResponse;
+      setQuote(randomQuote);
+      setIsLoading(false);
+    }
+    fetchQuote();
+  }, []);
+
+  return (
+    <div>
+      <p className="Loader" style={{ opacity: isLoading ? 1 : 0 }}>
+        Loading...
+      </p>
+      <h1>"{quote.quote}"</h1>
+     
+      <h2>- {quote.character}, {quote.title}</h2>
+    </div>
+  );
+}

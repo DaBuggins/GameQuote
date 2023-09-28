463ae29ed02c5b471b2c9a98b28b345c0a79cd0b
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2023-09-28 10:29:10 +0100

    Create Quoter.jsx
    refresh button

---
+++ Quoter.jsx
@@ -0,0 +1,12 @@
+import GameQuote from "./GameQuote";
+
+export default function Quoter() {
+
+  return (
+    <div>
+      <GameQuote />
+      <form action=""><button>NEXT</button></form>
+
+    </div>
+  );
+}
